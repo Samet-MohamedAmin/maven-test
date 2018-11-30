f5521e5e3c4f1d09a931c25b414c7f4674f752f4
Samet-MohamedAmin <mohamedamin.samet@yandex.com>
2018-11-30 01:12:41 +0100

    Update App.java

--- src/main/java/com/demo/app/App.java
+++ src/main/java/com/demo/app/App.java
@@ -8,8 +8,8 @@ public final class App {
     
     private static final Logger logger = LogManager.getLogger("HelloWorld");
 
-    static void testMethod(java.lang.Object a, java.lang.Object b){
-        logger.info("This is working %s",  a*b);
+    static void testMethod(int a, int b){
+        logger.info("This is working %d",  a*b);
     }
     public static void main(String[] args) {
         testMethod(1, 3);
