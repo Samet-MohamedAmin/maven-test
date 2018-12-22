392f93d5bccfd887bfbe7a22596cd4537276e185
mohamedamin <mohamedamin.samet@yandex.com>
2018-12-22 18:16:23 +0100

    enable external tomcat

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,14 +3,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.apache.log4j.Logger;
 
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
-	final static Logger logger = Logger.getLogger(DataLoader.class);
+	final static Logger logger = Logger.getLogger(DemoApplication.class);
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
 
 	public static void main(String[] args) {
 
@@ -18,3 +24,5 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
+
+
